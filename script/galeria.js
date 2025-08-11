dd8b8679f24420866f022c47dd9f50d591014976
Gus1331 <gustavooliver.souza@gmail.com>
2025-08-11 17:48:13 -0300

    feat: galeria feed

--- script/galeria.js
+++ script/galeria.js
@@ -1,20 +1,39 @@
 const filters = document.querySelectorAll('.filter');
+const images = document.querySelectorAll('.image');
+const f1 = document.querySelectorAll('.f1');
+const f2 = document.querySelectorAll('.f2');
+const f3 = document.querySelectorAll('.f3');
+const f4 = document.querySelectorAll('.f4');
 
-function filter (id){
+function filter(id){
     filters.forEach((f) => {
         f.classList.remove('active');
     });
     filters[id].classList.add('active');
+    images.forEach(hide)
     switch (id){
         case 0:
+            images.forEach(show);
             break;
         case 1:
+            f1.forEach(show);
             break;
         case 2:
+            f2.forEach(show);
             break;
         case 3:
+            f3.forEach(show);
             break;
         case 4:
+            f4.forEach(show);
             break;
     }
+}
+
+function hide(content){
+   content.classList.add('hidden');
+}
+
+function show(content){
+    content.classList.remove('hidden');
 }
